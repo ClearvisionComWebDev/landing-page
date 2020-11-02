4d38a01b8c0ed82c6f8c9f1553c66f1e21f97dd6
Jullan Quevedo <jullan.quevedo50@gmail.com>
2020-11-03 12:15:26 +1300

    add changes

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -6,7 +6,9 @@ const Footer = () => (
   <footer id="footer" className="wrapper style1-alt">
     <div className="inner">
       <ul className="menu">
-        <li>&copy; 2020 R and D Team All rights reserved.</li>
+        <div style={{textAlign: 'center'}}>
+        <li>&copy; 2020 Research and Development TeamClearVision All rights reserved.</li>
+        </div>
       </ul>
     </div>
   </footer>
--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -6,7 +6,7 @@ const Introduction = () =>
   <section id="intro" className="wrapper style1 fullscreen fade-up">
     <div className="inner">
       <div className="logo">
-         <img src={logo} style={{width:"457px",marginLeft:"250px" }} alt="Logo"/>
+         <img src={logo} style={{width:"457px" }} alt="Logo"/>
       </div>
       <h2>
         Delivering Quality Services to the Home and Businesses of many
