b9bc3a02c7c07194adba3a51558b620984590f7e
Jullan Quevedo <jullan.quevedo50@gmail.com>
2020-11-06 15:04:59 +1300

    fix

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,58 +1,22 @@
 import React from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-
 
 const ContactForm = () =>
-AOS.init();
   <section>
-
     <form method="post" action="#">
       <div className="fields">
         <div className="field half">
-        <div data-aos="fade-right"
-    data-aos-offset="200"
-    data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
-    data-aos-anchor-placement="top-center"
-     >   
           <label htmlFor="name" style={{color:"white"}}>Name</label>
           <input type="text" placeholder="Your Name"  name="name" id="name" style={{boxShadow:"rgba(0,0,0, 1.2) 0px 0px 8px"}}/>
-          </div>
         </div>
         <div className="field half">
-        <div data-aos="fade-right"
-    data-aos-offset="200"
-    data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
-    data-aos-anchor-placement="top-center"
-     >   
           <label htmlFor="email" style={{color:"black"}}>Email</label>
           <input type="text" placeholder="Enter your email" name="email" id="email" style={{boxShadow:"rgba(0,0,0, 1.2) 0px 0px 8px"}}/>
           </div>
-        </div>
         <div className="field">
-        <div data-aos="fade-right"
-    data-aos-offset="200"
-    data-aos-delay="50"
-    data-aos-duration="1000"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
-    data-aos-anchor-placement="top-center"
-        >   
           <label htmlFor="message"style={{color:"white"}}>Message</label>
           <textarea name="message" placeholder="What would you like to know" id="message" rows="5" style={{boxShadow:"rgba(0,0,0, 1.2) 0px 0px 8px"}}/>
           </div>
         </div>
-      </div>
       <ul className="actions">
         <li>
           <a href="/#" className="button submit" >
@@ -62,5 +26,7 @@ AOS.init();
       </ul>
     </form>
   </section>
+
+
 
 export default ContactForm;
--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -2,30 +2,25 @@ import React from 'react';
 import logo from './img/logo.png';
 import image from './img/image1.jpg';
 import scrollTo from 'gatsby-plugin-smoothscroll';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 
 const Introduction = () =>
-AOS.init();
+
   <section id="intro" className="wrapper style1 fullscreen fade-up" style={{backgroundImage: `url(${image})`, borderBottom:"5px solid #00142e"}}>
       <div className="inner">
       <div className="logo">
          <img src={logo} style={{width:"457px", marginLeft:"-60px" }} alt="Logo"/>
       </div>  
-
-      <div data-aos="fade-bottom">
       <h2 style={{color: "white", fontFamily:"Helvetica", textShadow:"0 2px 5px rgba(0,0,0,.5)",boxShadow:"0 2px 25px rgba(0,0,0,1.2)",padding:"20px",backgroundColor:"rgb(7, 29, 59,0.7)"}}>
         Delivering Quality Services to Kiwis Homes and Businesses
        </h2>
        </div>
         <div>
-          <button  style={{backgroundColor: "transparent"}} onClick={() => scrollTo('#one')}>Learn More</button>
+          <button  style={{backgroundColor: "transparent",marginLeft:"80px"}} onClick={() => scrollTo('#one')}>Learn More</button>
           <button style={{backgroundColor:"transparent",marginLeft:"30px"}} onClick={() => scrollTo('#four')}>Get in Touch</button>
         </div>
         <div>
         </div>
-    </div>
   </section>
 
 
