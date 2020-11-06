ea90f30ccc834ceb10cd599d0f6e5a0676d188b1
Jullan Quevedo <jullan.quevedo50@gmail.com>
2020-11-06 14:10:18 +1300

    add animations

--- src/components/capabilities/Capability.js
+++ src/components/capabilities/Capability.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-const Capability = ({ heading, description, iconClass }) =>
+const Capability = ({ heading, description }) =>
   <section>
-    <span className={`icon major ${iconClass}`} />
     <h3>{heading}</h3>
     <p>{description}</p>
   </section>
